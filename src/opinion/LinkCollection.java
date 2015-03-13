472a07df8ef88ab674ba988708ec448e7b540cb6
Abu Shahriar Ratul <ratulcse10@gmail.com>
2015-03-13 22:11:10 +0600

    Opinion Add

---
+++ src/opinion/LinkCollection.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package opinion;
+
+import education.*;
+import sports.*;
+import CustomExceptions.TerminateException;
+import Storage.Connect;
+import static Storage.Connect.insertData;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import testparser.Onepage;
+import testparser.Testparser;
+
+/**
+ *
+ * @author yousufkhan
+ */
+public class LinkCollection {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<String> dataLinks = new ArrayList<String>();
+        boolean flag = true;
+        int i = 1;
+
+        try {
+            Connection connection = OpinionConnect.CreateConntection();
+            try {
+                Statement stmtement = OpinionConnect.CreateStatement(connection);
+
+                while (flag) {
+
+                    try {
+                        dataLinks = Onepage.getLinks("http://www.prothom-alo.com/opinion", "" + i);
+
+                    } catch (Exception e) {
+                        System.out.println("terminate");
+                        flag = false;
+                        break;
+                    }
+
+                    for (String link : dataLinks) {
+                        try {
+                            OpinionConnect.insertData(stmtement, link);
+                            connection.commit();
+                            System.out.println(link);
+                        } catch (Exception e) {
+                            flag = false;
+                        }
+                    }
+                    i++;
+
+                }
+
+                stmtement.close();
+                connection.commit();
+                connection.close();
+            } catch (Exception e) {
+                connection.close();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Failed to create connection.");
+        }
+
+    }
+
+}
